ab3ee9891c025a57d6b0382105144f30f10873d6
FelicityS1 <lr.fdsario@mmdc.mcl.edu.ph>
2025-02-10 00:51:34 +0800

    Signup Loading Page

--- src/index.js
+++ src/index.js
@@ -63,6 +63,7 @@ app.post("/signup", async (req, res) => {
 
         const userdata = await collection.insertMany(data);
         console.log(userdata);
+        res.render("loginloading", { message: "User Added. Redirecting to LoginPage..." });
     }
 
 });
