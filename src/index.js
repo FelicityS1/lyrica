bc6799b472953ef1a50270dbd98484739d02a93f
FelicityS1 <lr.fdsario@mmdc.mcl.edu.ph>
2025-02-14 00:15:23 +0800

    Porting

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({extended: false}));
 
 app.set('views engine', 'ejs');
 
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
