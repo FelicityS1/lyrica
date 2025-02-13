2f4f06ce1bb7419a98ed66e4e06171a29ea7a3e9
FelicityS1 <lr.fdsario@mmdc.mcl.edu.ph>
2025-02-14 00:13:05 +0800

    Viewss

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: false}));
 
-app.set('view engine', 'ejs');
+app.set('views engine', 'ejs');
 
 app.use(express.static("public"));
 
