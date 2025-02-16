e5ff5b5a47d50b07cd7591f8a277a9f8092ddc47
FelicityS1 <lr.fdsario@mmdc.mcl.edu.ph>
2025-02-16 17:30:10 +0800

    Index

--- src/index.js
+++ src/index.js
@@ -14,9 +14,6 @@ app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 
-// Set the views directory explicitly (important for Render deployment)
-app.set("views", path.join(__dirname, "views"));
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
