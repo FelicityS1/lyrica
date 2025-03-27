53b6e1a7d8260c5d28f5e1a554c07f550c885c1a
FelicityS1 <lr.fdsario@mmdc.mcl.edu.ph>
2025-03-27 17:32:22 +0800

    Remove dupe

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const { name } = require("ejs");
 const mongoose = require("mongoose");
-const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://Lizzy123:Testing123abc@lyricadb.mngvl.mongodb.net/LyricaDB", {
     useNewUrlParser: true,
